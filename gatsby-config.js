224488f18580d7f456deb4698186660b2355a437
Alejandro Vera <aleverasj@gmail.com>
2022-05-06 17:30:02 -0300

    Parte final del tutorial parte 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,17 @@
 module.exports = {
   siteMetadata: {
+    title: "Mi primer sitio como programador web",
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [  
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 }
